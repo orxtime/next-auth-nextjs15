58b60afe76a0d65185835cfd77978e15fca4e2da
anushujan satkunarasa <43997407+anushujan@users.noreply.github.com>
2024-11-08 22:28:24 +0530

    first commit

---
+++ src/lib/mongodb.ts
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+export const connectMongoDB = async (): Promise<void> => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI as string);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("Error connecting to MongoDB: ", error);
+  }
+};
